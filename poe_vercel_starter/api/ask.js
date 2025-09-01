82a9d1475dcfc4700516af2b96f774321b46d1ef
irun7s <wangshuo4474@gmail.com>
2025-09-02 04:10:11 +0800

    Update ask.js

--- poe_vercel_starter/api/ask.js
+++ poe_vercel_starter/api/ask.js
@@ -1,6 +1,4 @@
-export const config = {
-  runtime: 'nodejs20'
-};
+export const config = { runtime: 'nodejs20.x' };
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
